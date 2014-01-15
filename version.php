2d215b5200005754db26b1e8a8afdc11103b19a5
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-01-15 15:45:14 +0000

    Update version.php for the 3.2 release.

--- version.php
+++ version.php
@@ -25,13 +25,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2013070900;
+$plugin->version   = 2014011500;
 $plugin->requires  = 2012120300;
 $plugin->cron      = 0;
 $plugin->component = 'qformat_stack';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1 for Moodle 2.4+';
+$plugin->release = '3.2 for Moodle 2.4+';
 
 $plugin->dependencies = array(
-    'qtype_stack' => 2013070900,
+    'qtype_stack' => 2014011500,
 );
